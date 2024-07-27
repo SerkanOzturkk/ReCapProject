fc43a7a748ab938ba21b1d3eebf73bc3a7613e9e
Serkan Öztürk <serkanoztrk237@gmail.com>
2024-07-27 21:17:11 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+
+CarManager carManager = new CarManager(new EfCarDal());
+
+/*
+carManager.Add(new Car()
+{
+    BrandId = 1,
+    CarName = "Aston Martin",
+    ColorId = 2,
+    DailyPrice = 500,
+    Description = "fancy car",
+    ModelYear = "2020"
+});*/
+
+/*carManager.Update(new Car()
+{
+    CarId = 6,
+    BrandId = 1,
+    CarName = "test",
+    ColorId = 2,
+    DailyPrice = 500,
+    Description = "fancy car",
+    ModelYear = "2024"
+});*/
+
+/*carManager.Delete(new Car()
+{
+    CarId = 6,
+    BrandId = 1,
+    CarName = "test",
+    ColorId = 2,
+    DailyPrice = 500,
+    Description = "fancy car"
+    
+});*/
+
+
+
+
+foreach (var car in carManager.GetAll())
+{
+    Console.WriteLine(car.CarId+" ----- " + car.CarName + " ----- " + car.Description + " ----- " + car.ModelYear);
+}
+
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId = 1,BrandId = 1,ColorId = 1,ModelYear = "2015",DailyPrice = 20.000,Description = "Fiat"},
+                new Car{CarId = 2,BrandId = 1,ColorId = 1,ModelYear = "2018",DailyPrice = 30.000,Description = "Opel"},
+                new Car{CarId = 3,BrandId = 2,ColorId = 2,ModelYear = "2023",DailyPrice = 55.000,Description = "Mercedes"},
+                new Car{CarId = 4,BrandId = 2,ColorId = 2,ModelYear = "2022",DailyPrice = 45.000,Description = "Bmw"},
+                new Car{CarId = 5,BrandId = 3,ColorId = 2,ModelYear = "2021",DailyPrice = 40.000,Description = "Renault"}
+            };
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void GetById(int CarId)
+        {
+            var wantedCar = _cars.Where(c => c.CarId == CarId);
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+    }
+}

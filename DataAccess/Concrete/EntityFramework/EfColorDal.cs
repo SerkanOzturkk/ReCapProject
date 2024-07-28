a7e4234c5f8e5ba08c200b10c7c8876cbf1c1962
Serkan Öztürk <serkanoztrk237@gmail.com>
2024-07-28 16:26:38 +0300

    Core layer configuration

--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -7,57 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
+using Core.DataAccess.EntityFramework;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfColorDal : IColorDal
+    public class EfColorDal : EfEntityRepositoryBase<Color, CarDbContext>, IColorDal
     {
-        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
-        {
-            using (CarDbContext context = new CarDbContext())
-            {
-                return filter == null
-                    ? context.Set<Color>().ToList()
-                    : context.Set<Color>().Where(filter).ToList();
-            }
-        }
-
-        public Color Get(Expression<Func<Color, bool>> filter)
-        {
-            using (CarDbContext context = new CarDbContext())
-            {
-                return context.Set<Color>().SingleOrDefault(filter);
-            }
-        }
-
-        public void Add(Color entity)
-        {
-            using (CarDbContext context = new CarDbContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Update(Color entity)
-        {
-            using (CarDbContext context = new CarDbContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Color entity)
-        {
-            using (CarDbContext context = new CarDbContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
+        
     }
 }
